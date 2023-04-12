91effdb54a7fbf81416299f3d4a3932f19214ee2
satarzade <alisattarzadeh71@gmail.com>
2023-04-13 00:11:17 +0430

    first commit

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -20,8 +20,8 @@ const Slider: React.FC<ISliderProps> = () => {
         <>
             <Swiper
                 // style={{
-                //   "--swiper-navigation-color": "#fff",
-                //   "--swiper-pagination-color": "#fff",
+                //     "--swiper-navigation-color": "#fff",
+                //     "--swiper-pagination-color": "#fff",
                 // }}
                 loop={true}
                 spaceBetween={10}
@@ -31,7 +31,48 @@ const Slider: React.FC<ISliderProps> = () => {
                 className="mySwiper2"
             >
                 <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                    <img src="/photos/22مرداد چپ دست ها.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                </SwiperSlide>
+            </Swiper>
+            <Swiper
+                onSwiper={setThumbsSwiper}
+                loop={true}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src="/photos/22مرداد چپ دست ها.jpg" />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
